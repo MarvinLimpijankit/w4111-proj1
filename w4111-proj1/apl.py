4c3d42fd4a0874eb2b4613d98e3de63eb046ea41
Maximo Oen <mmo2134@columbia.edu>
2021-11-14 21:39:41 -0500

    Added cuisine in desc

--- w4111-proj1/apl.py
+++ w4111-proj1/apl.py
@@ -17,8 +17,14 @@ def home():
 #user can label a restaurant as visited or wishlisted
     g.conn = engine.connect()
     restaurants = g.conn.execute(
-        "SELECT * FROM restaurants r\
-        LEFT JOIN addresses a ON r.a_id = a.a_id",
+        "SELECT r.*, a.*, cui.cuisine FROM restaurants r\
+        LEFT JOIN addresses a ON r.a_id = a.a_id\
+        LEFT JOIN ( SELECT re.r_id, STRING_AGG(c.cuisine_name, \', \') as cuisine\
+        FROM restaurants re\
+        LEFT JOIN is_cuisine ic ON ic.r_id = re.r_id\
+        LEFT JOIN cuisines c ON ic.c_id = c.c_id\
+        GROUP BY re.r_id) as cui\
+        ON r.r_id = cui.r_id",
     ).fetchall()
 
     #what has the user already visited
@@ -54,11 +60,20 @@ def home():
 
 @bp.route('/profile')
 def profile():
+    g.conn = engine.connect()
+
 #show user current reservations
+    visited = g.conn.execute(
+        "SELECT r.r_id, r.restaurant_name FROM users u\
+        LEFT JOIN visited v ON u.u_id = v.u_id\
+        LEFT JOIN restaurants r ON v.r_id = r.r_id\
+        WHERE u.u_id = %s", (g.user['u_id'])
+    ).fetchall()
+
 #show users visited list
 #show user wishlist
 #show list of followed users
-    return render_template('index')
+    return render_template('profile.html', user=g.user, )
 
 
 #add routing reviewing, reservations, visited, wishlist
